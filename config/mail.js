74099aaef64cfb8e61c294b51a14474a2d38b53b
Bhargavi <bhargavi.t@dhatvibs.com>
2026-05-29 11:28:03 +0530

    mail code updtae

--- config/mail.js
+++ config/mail.js
@@ -2,13 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
+
+  family: 4, // FORCE IPv4
 });
 
 transporter.verify((error, success) => {
